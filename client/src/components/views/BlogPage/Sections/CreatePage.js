8df2605febdef73c0f9a87400e5fde162782ccf6
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-01-10 01:40:41 -0800

    Importing Components
    Importing components (postpage, blogpage and createblogpage) to the app.js file. This will import the components to the main "container" of the app.

--- client/src/components/views/BlogPage/Sections/CreatePage.js
+++ client/src/components/views/BlogPage/Sections/CreatePage.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography, Button, Form, message } from 'antd';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+
+const { Title } = Typography;
 
 function CreatePage() {
+  const user = useSelector(state => state.user);
+  const [content, setContent] = useState('');
+
+  const onSubmit = event => {
+    event.preventDefault();
+
+    setContent('');
+
+    if (user.userData && !user.userData.isAuth) {
+      return alert('Please Login In');
+    }
+
+    const variables = {
+      content: content,
+      userID: user.userData._id
+    };
+    axios.post('api/blog/createPost', variables).then();
+  };
   return (
     <div>
       <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
         <div style={{ textAlign: 'center' }}>
           <Title level={2}> Editor </Title>
         </div>
-        <QuillEditor
+
+        {/* <QuillEditor
           placeholder={'Start Posting'}
           onEditorChange={onEditorChange}
           onFilesChange={onFilesChange}
-        />
+        /> */}
 
         <Form onSubmit={onSubmit}>
           <div style={{ textAlign: 'center', margin: '2rem' }}>
